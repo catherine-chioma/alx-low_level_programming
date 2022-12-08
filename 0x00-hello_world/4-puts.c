0fc204e0d67ac061c5d17e2f22fcdf0b897b1d59
catherine-chioma <chi.ihuaku@gmail.com>
2022-12-08 13:24:06 -0800

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio>
+/*
+ * main - print a line of code using puts
+ *
+ *  Return: o
+ */
+int main(void)
+{
+	puts("/"Programming is like building a multilingual puzzle")
+		return (o);
+}
