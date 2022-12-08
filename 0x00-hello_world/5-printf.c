99d759024621803e17b62a64a1f0168a79c5ea04
catherine-chioma <chi.ihuaku@gmail.com>
2022-12-08 15:43:58 -0800

    My second c programming

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -print a c programming using printf
+ * enter
+ * Return: o
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (o);
+}
