b3d96eb0c5dc9c8c7f8f7f228a6473402306fc96
catherine-chioma <chi.ihuaku@gmail.com>
2022-12-08 23:06:23 -0800

    My second c programming

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 
-
 /**
- *  main - Entry
+ * main - Entry
  *
- *  Return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
-gcc 5-printf.c -o 5-printf
-cat ./5-printf
