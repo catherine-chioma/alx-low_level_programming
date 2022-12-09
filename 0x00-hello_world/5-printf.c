2edba81a10bfafa5ef8cdbf2e055c040a5e155b0
catherine-chioma <chi.ihuaku@gmail.com>
2022-12-08 22:24:04 -0800

    /** * main - Entry * * Return: Always 0 (Success) */ int main(void) {  printf("with proper grammar, but the outcome is a piece of art,\n"); return (0); }

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
+
+
 /**
- * main -print a c programming using printf
- * enter
- * Return: o
+ *  main - Entry
+ *
+ *  Return: Always 0 (Success)
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (o);
+	return (0);
 }
+gcc 5-printf.c -o 5-printf
+cat ./5-printf
