79cf3f0800b391cfed7c7a2d2a6f3145450c48f7
catherine-chioma <chi.ihuaku@gmail.com>
2022-12-09 11:46:57 -0800

    My UNIX task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- *  main - Entry
- * Return: 1
+ * main - Entry
+ *  Return: 1
  */
 int main(void)
 {
-	write(and that piece of art is useful" - Dora Korpar, 2015-10-19);
-	return (1);
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 }
