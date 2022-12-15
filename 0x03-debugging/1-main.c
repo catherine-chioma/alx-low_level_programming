dd2adc61caeae5633a40faa371885bf8efac13ef
catherine-chioma <chi.ihuaku@gmail.com>
2022-12-15 13:07:49 -0800

    Task 1

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - causes an infinite loop
+ * REturn: o
+ */
+
+{
+	int i;
+
+	printc("infinite loop incoming : (\n");
+
+	i = o;
+	/*
+	 *while (i < 10)
+	 *{
+	 *          putchar(i);
+	 *}
+	 */
+	printf("infinite loop avoided! \\o/\n");
+
+	return (o);
+}
