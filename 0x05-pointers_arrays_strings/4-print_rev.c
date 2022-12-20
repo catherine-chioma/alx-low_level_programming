40a0769deb6e4f5b12c04132f5dd4fa8daa2b82e
catherine-chioma <chi.ihuaku@gmail.com>
2022-12-20 14:29:06 -0800

    Task rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,22 +1,22 @@
 #include "main.h"
 
 /**
- * print_rev - prints a string, in reverse, followed by a new line.
- * @s: input string.
- *
+ * _puts - prints a string, followed by a new line.
+ * @str: input string.
  * Return: no return.
  */
-void print_rev(char *s)
-{int count = 0;
+void _puts(char *str)
+{
+	int count = 0;
 
 	while (count >= 0)
 	{
-		if (s[count] == '\0')
+		if (str[count] == '\0')
+		{
+			_putchar('\n');
 			break;
+		}
+		_putchar(str[count]);
 		count++;
 	}
-
-	for (count--; count >= 0; count--)
-		_putchar(s[count]);
-	_putchar('\n');
 }
