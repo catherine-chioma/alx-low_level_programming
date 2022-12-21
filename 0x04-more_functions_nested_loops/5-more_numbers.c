e4519200d13f0926b50377fb409f486fee8cb4a0
catherine-chioma <chi.ihuaku@gmail.com>
2022-12-21 13:53:22 -0800

    Task more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -19,15 +19,8 @@ void more_numbers(void)
 			{
 				_putchar((j / 10) + 48);
 			}
-			_putchar((j % 10) + 48);
+			_putchar('\n');
+
+			i++;
 		}
-
-		_putchar( '\n' );
-
-		i++;
 	}
-}
-
-				
-		
-		
