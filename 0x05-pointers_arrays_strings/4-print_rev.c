d9b9a019886b4272db41c28a693c358792f40191
catherine-chioma <chi.ihuaku@gmail.com>
2022-12-23 22:53:09 -0800

    Task rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,13 +10,13 @@ void print_rev(char *s)
 	int i = 0;
 	int j;
 
-		while (s[i] != '\0')
-		{
-			i++;
-		}
+	while (s[i] != '\0')
+	{
+		i++;
+	}
 	for (j = i - 1; j >= 0; j--)
 	{
-		_putchar (s[j]);
+		putchar(s[j]);
 	}
 	_putchar('\n');
 }
