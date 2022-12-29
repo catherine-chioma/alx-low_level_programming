e8bb11cfdd30dbfabc272837efc57b28d060ca81
catherine-chioma <chi.ihuaku@gmail.com>
2022-12-29 14:20:48 -0800

    Task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,7 +10,7 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-		_putchar(45);
+		putchar(45);
 		i = n * -1;
 	}
 	else
@@ -29,6 +29,6 @@ void print_number(int n)
 
 	for (; count >= 1; count /= 10)
 	{
-		_putchar(((i / count) % 10) + 48);
+		putchar(((i / count) % 10) + 48);
 	}
 }
