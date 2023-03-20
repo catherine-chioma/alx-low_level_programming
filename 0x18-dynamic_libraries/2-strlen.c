d0def1dbc8374a0f824a54c6a12f2641a50efbd1
catherine-chioma <chi.ihuaku@gmail.com>
2023-03-20 08:29:54 -0700

    Task o

---
+++ 0x18-dynamic_libraries/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - A function that returns the length of a string.
+ * @s: string pointer to pass to the pointer
+ * Return: return length of the string
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
